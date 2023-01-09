a2a94065b38df05cab10e6b57cc7fa8ebabb3d50
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-01-09 23:20:43 +0900

    検索機能の実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
@@ -29,9 +27,9 @@ class PixabayPage extends StatefulWidget {
 class _PixabayPageState extends State<PixabayPage> {
   List hits = [];
 
-  Future<void> fetchImages() async {
+  Future<void> fetchImages({required String text}) async {
     Response response = await Dio().get(
-        "https://pixabay.com/api/?key=32688940-599bdd8b91299e2e22675b480&q=yellow+flowers&image_type=photo");
+        "https://pixabay.com/api/?key=32688940-599bdd8b91299e2e22675b480&q=$text&image_type=photo&per_page=100");
     hits = response.data['hits'];
     setState(() {});
   }
@@ -40,13 +38,25 @@ class _PixabayPageState extends State<PixabayPage> {
   void initState() {
     super.initState();
     //最初に一度だけ呼ばれる
-    fetchImages();
+    fetchImages(text: "programing");
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+          title: TextFormField(
+            initialValue: "programing",
+            decoration: const InputDecoration(
+              fillColor: Colors.white,
+              filled: true,
+            ),
+            onFieldSubmitted: (text) {
+              print(text);
+              fetchImages(text: text);
+            },
+          ),
+          backgroundColor: Colors.cyan),
       body: GridView.builder(
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 3),
